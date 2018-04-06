9be7128498b40851fdbedd132bf97528f57c791e
Ihor Bondar <ihor.bondar92@gmail.com>
2018-04-06 18:42:13 +0300

    Add src to module directories

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -43,6 +43,7 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx'],
+    modules: [path.resolve(__dirname, '../src'), 'node_modules'],
   },
   plugins: [
     new HtmlWebpackPlugin({
