582b7b28b27a7fb22cb962ea7a9be2cf35443221
Ihor Bondar <ihor.bondar92@gmail.com>
2018-04-06 20:32:15 +0300

    Configure jest for coverage reports

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   setupTestFrameworkScriptFile: '<rootDir>src/support/setupTests.js',
+  collectCoverage: true,
+  coverageReporters: ['lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
+  collectCoverageFrom: ['./src/**/*.{js,jsx}'],
 };
