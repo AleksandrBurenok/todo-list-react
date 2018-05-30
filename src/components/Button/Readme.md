37c2f01fc339add905b5ec9e887f2bc7222cba77
Ihor Bondar <ihor.bondar92@gmail.com>
2018-05-30 19:27:21 +0300

    Add button danger accent color:

--- src/components/Button/Readme.md
+++ src/components/Button/Readme.md
@@ -4,6 +4,10 @@ Default button:
 <Button onClick={()=>{alert('Clicked')}}>Click me!</Button>  
 ```
 
+```jsx harmony
+<Button accentColor="danger" onClick={()=>{alert('Clicked')}}>Click me!</Button>  
+```
+
 Disabled button:
 ```jsx harmony
 <Button disabled={true} onClick={()=>{alert('Clicked')}}>Click me!</Button>  
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
 import './Button.scss';
 
-const Button = ({ children, onClick, disabled }) => (
-  <button
-    className="Button"
-    disabled={disabled}
-    onClick={onClick}
-  >
-    {children}
-  </button>
-);
+const Button = ({ children, onClick, disabled, accentColor }) => {
+  const buttonClassName = classNames('Button', {
+    [`Button--${accentColor}`]: Boolean(accentColor),
+  });
+
+  return (
+    <button
+      className={buttonClassName}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
   onClick: PropTypes.func,
   disabled: PropTypes.bool,
+  accentColor: PropTypes.string,
 };
 
 Button.defaultProps = {
   onClick: null,
   disabled: false,
+  accentColor: '',
 };
 
 export default Button;
