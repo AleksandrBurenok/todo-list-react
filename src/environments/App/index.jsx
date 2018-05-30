be9f4e2a6c74627181a8ad15f8f95ac00e8d28b6
Ihor Bondar <ihor.bondar92@gmail.com>
2018-05-30 19:55:50 +0300

    Create App layout

--- src/environments/App/index.jsx
+++ src/environments/App/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import 'assets/styles/global.scss';
 
-const App = () => <h1>Welcome</h1>;
+import './App.scss';
+
+const App = () => (
+  <main className="App">
+    <section className="App-form">
+      <h2>Form</h2>
+    </section>
+    <section className="App-content">
+      <h2>Content</h2>
+    </section>
+  </main>
+);
 
 ReactDOM.render(<App />, global.document.querySelector('#app'));
