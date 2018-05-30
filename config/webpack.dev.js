3ea11420a1c9a71e7b6a04b36eee05c071da6fda
Ihor Bondar <ihor.bondar92@gmail.com>
2018-05-30 12:02:57 +0300

    Add webpack dashboard plugin

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const DashboardPlugin = require('webpack-dashboard/plugin');
 const merge = require('webpack-merge');
 const commonConfig = require('./webpack.common');
 
@@ -12,8 +13,11 @@ module.exports = merge(commonConfig, {
     port: 9000,
   },
   mode: 'development',
-  plugins: [new webpack.DefinePlugin({
-    'process.env.NODE_ENV':
-      JSON.stringify('development'),
-  })],
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV':
+        JSON.stringify('development'),
+    }),
+    new DashboardPlugin(),
+  ],
 });
