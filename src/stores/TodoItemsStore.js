a0cd2f492fa1ee3bd939220b277962895f3e267c
Ihor Bondar <ihor.bondar92@gmail.com>
2018-05-30 19:52:06 +0300

    Create TodoItems store

---
+++ src/stores/TodoItemsStore.js
@@ -0,0 +1,21 @@
+import { observable, action } from 'mobx';
+import UUID from 'uuid/v4';
+import TodoItem from '../models/TodoItem';
+
+export default class TodoItemsStore {
+  @observable todoItems = [];
+
+  @action('Add Todo Item')
+  addTodo = (payload) => {
+    const todoItem = (payload instanceof TodoItem) ? payload : new TodoItem(payload);
+    todoItem.id = UUID();
+    this.todoItems.push(todoItem);
+    return this;
+  };
+
+  @action('Remove Todo Item')
+  removeTodo = (todoItem) => {
+    this.todoItems = this.todoItems.filter(anItem => anItem.id !== todoItem.id);
+    return this;
+  };
+}
